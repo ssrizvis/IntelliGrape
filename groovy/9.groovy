3ec0980e807b436812e9599a9ff4504156a77073
ssrizvis <ssrizvis@gmail.com>
2015-05-11 19:12:35 +0530

    Update 9.groovy

--- groovy/9.groovy
+++ groovy/9.groovy
@@ -1,5 +1,5 @@
-def file=new File('aroop.txt')
-def nfile=new File('new2.txt')
+def file=new File('syed.txt')
+def nfile=new File('shahzad.txt')
 def x
 file.eachLine{line->print line
    x=line.replaceAll("\\s","")
